3b683e098c806d0aa3aecfc3016459940b914bbc
Adithyakb2005 <adithyakb1001@gmail.com>
2025-01-14 11:44:16 +0530

    almost completed

---
+++ phonebook/c_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('', views.list_contacts, name='list_contacts'),  
+    path('contacts/', RedirectView.as_view(url='/', permanent=False)), 
+    path('add/', views.add_contact, name='add_contact'),
+    path('<int:pk>/', views.view_contact, name='default_view_contact'),
+
+    path('<int:pk>/edit/', views.edit_contact, name='edit_contact'),
+    path('<int:pk>/delete/', views.delete_contact, name='delete_contact'),
+    path('<int:pk>/call/', views.call_contact, name='call_contact'),
+]
---
+++ phonebook/c_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import  *
+from .forms import *
+# Create your views here.
+
+def add_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('list_contacts')
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/add_contact.html', {'form': form})
+
+def list_contacts(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/list_contacts.html', {'contacts': contacts})
+
+def view_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contacts/view_contact.html', {'contact': contact})
+
+def edit_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('view_contact', pk=pk)
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/edit_contact.html', {'form': form, 'contact': contact})
+
+def delete_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    contact.delete()
+    return redirect('list_contacts')
+
+def call_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contacts/call_contact.html', {'contact': contact})
